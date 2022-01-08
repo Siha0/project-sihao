3db484b973b79f702145775dcce72b426a5d99a8
Siha0 <96207639+Siha0@users.noreply.github.com>
2022-01-08 18:05:39 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,13 +61,13 @@ function draw() {
 
     //uncomment the correct switch statement
 
-             switch(select_balloon ){
-             case 1: redBalloon();
-             case 2:blueBalloon();
-             case 3:pinkBalloon();
-             case 4:greenBalloon();
-             default:break;
-             }
+             // switch(select_balloon ){
+             // case 1: redBalloon();
+             // case 2:blueBalloon();
+             // case 3:pinkBalloon();
+             // case 4:greenBalloon();
+             // default:break;
+             //}
 
 
              switch(select_balloon ){
@@ -83,17 +83,17 @@ function draw() {
              }
 
 
-             switch(select_balloon ){
-             case 1: redBalloon();
-             break;
-             case 1:blueBalloon();
-             break;
-             case 1:pinkBalloon();
-             break;
-             case 1:greenBalloon();
-             break;
-             default:break;
-             }
+             // switch(select_balloon ){
+             // case 1: redBalloon();
+             // break;
+             // case 1:blueBalloon();
+             // break;
+             // case 1:pinkBalloon();
+             // break;
+             // case 1:greenBalloon();
+             // break;
+             // default:break;
+             //}
 
 }
     
